66853cc421ae6b73c7cf25d9b4b0386426dc677b
Andras Kadar <kadarandrass@gmail.com>
2021-10-19 09:28:29 +0200

    [SA-50840] Do not show the alert consent before SSO login on <iOS 13
    We got rid of the “annoying” prompt before showing the SSO login page on iOS 13+ by using the system-provided ephemeral browser session. Prior to iOS 13, it is not an option.
    
    Reading up on threads ([AppAuth-iOS#402](https://github.com/openid/AppAuth-iOS/issues/402)), showcasing a similar situation to ours, it seems like the only solution for us is to use the `SFSafariViewController` for authentication on all devices below iOS 13.

--- Source/AppAuth/iOS/OIDExternalUserAgentIOS.m
+++ Source/AppAuth/iOS/OIDExternalUserAgentIOS.m
@@ -87,8 +87,8 @@ NS_ASSUME_NONNULL_BEGIN
   BOOL openedUserAgent = NO;
   NSURL *requestURL = [request externalUserAgentRequestURL];
 
-  // iOS 12 and later, use ASWebAuthenticationSession
-  if (@available(iOS 12.0, *)) {
+  // iOS 13 and later, use ASWebAuthenticationSession with ephemeral web session
+  if (@available(iOS 13.0, *)) {
     // ASWebAuthenticationSession doesn't work with guided access (rdar://40809553)
     if (!UIAccessibilityIsGuidedAccessEnabled()) {
       __weak OIDExternalUserAgentIOS *weakSelf = self;
@@ -123,37 +123,7 @@ NS_ASSUME_NONNULL_BEGIN
       openedUserAgent = [authenticationVC start];
     }
   }
-  // iOS 11, use SFAuthenticationSession
-  if (@available(iOS 11.0, *)) {
-    // SFAuthenticationSession doesn't work with guided access (rdar://40809553)
-    if (!openedUserAgent && !UIAccessibilityIsGuidedAccessEnabled()) {
-      __weak OIDExternalUserAgentIOS *weakSelf = self;
-      NSString *redirectScheme = request.redirectScheme;
-      SFAuthenticationSession *authenticationVC =
-          [[SFAuthenticationSession alloc] initWithURL:requestURL
-                                     callbackURLScheme:redirectScheme
-                                     completionHandler:^(NSURL * _Nullable callbackURL,
-                                                         NSError * _Nullable error) {
-        __strong OIDExternalUserAgentIOS *strongSelf = weakSelf;
-        if (!strongSelf) {
-            return;
-        }
-        strongSelf->_authenticationVC = nil;
-        if (callbackURL) {
-          [strongSelf->_session resumeExternalUserAgentFlowWithURL:callbackURL];
-        } else {
-          NSError *safariError =
-              [OIDErrorUtilities errorWithCode:OIDErrorCodeUserCanceledAuthorizationFlow
-                               underlyingError:error
-                                   description:@"User cancelled."];
-          [strongSelf->_session failExternalUserAgentFlowWithError:safariError];
-        }
-      }];
-      _authenticationVC = authenticationVC;
-      openedUserAgent = [authenticationVC start];
-    }
-  }
-  // iOS 9 and 10, use SFSafariViewController
+  // iOS 9+, use SFSafariViewController
   if (@available(iOS 9.0, *)) {
     if (!openedUserAgent && _presentingViewController) {
       SFSafariViewController *safariVC =
